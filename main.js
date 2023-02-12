5a07d3010746d0602afa04e2f3a1b75a8f778db7
Lonthein <mail@mendax.one>
2023-02-12 19:30:26 +0100

    Setting up discord.js foundation

---
+++ main.js
@@ -0,0 +1,13 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const token = process.env.BOT_TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(token);
